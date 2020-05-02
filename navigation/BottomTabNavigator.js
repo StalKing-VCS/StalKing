f8de17655ff285461b16bc28ab07a187739308e3
kshitij.singh <kshitij.singh@go-jek.com>
2020-05-03 01:12:39 +0530

    Enables bottom navigation with icons

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {View,Text} from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+import {View, Text} from 'react-native';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 import Profile from '../screens/Profile';
 import Portfolio from '../screens/Portfolio';
+import Icon from "react-native-vector-icons/AntDesign";
 
-const BottomTabNavigator = createBottomTabNavigator({
-    Portfolio: Portfolio,
-    Profile: Profile
-});
+const BottomTabNavigator = createMaterialBottomTabNavigator({
+        Portfolio: {
+            screen: Portfolio,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name="wallet" size={25} color={tintColor}/>
+                )
+            }
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name="profile" size={25} color={tintColor}/>
+                )
+            }
+        }
+    },
+    {
+        initialRouteName: 'Portfolio',
+        tabBarOptions: {
+            activeTintColor: '#eb6e3d'
+        },
+        activeColor: '#F44336',
+    });
 
 export default BottomTabNavigator;
